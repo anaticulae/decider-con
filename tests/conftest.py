fef41868f09128043e2ae44ac73a51ea863756c5
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-12-18 02:53:43 +0100

    chore(resources): add test data generator

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,47 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import genex
+import power
+import pytest
+import utila
+
+import decider_con
+
 pytest_plugins = ['pytester', 'xdist']  # pylint: disable=invalid-name
+
+PACKAGE = decider_con.PROCESS
+power.setup(decider_con.ROOT)
+
+RESOURCES = [
+    (power.MASTER072_PDF, '0:10'),
+]
+
+WORKER = 6
+
+
+@pytest.mark.usefixtures('session')
+def pytest_sessionstart():
+    power.run([power.generated()])
+
+
+def extract(resources):
+    # ensure to handle single file generation or common resource subfolder
+    # correctly. To determine the output path it is required to determine
+    # the parent path of at least two files. If resources provide only a
+    # single file the parental determination is not possible. Therefore we
+    # have to add the data root of all test files.
+    utila.log(f'root: {power.REPOSITORY}')
+    resources.append(power.REPOSITORY)
+
+    genex.extract(
+        files=resources,
+        destination=power.generated(),
+        groupme=True,
+        sections=True,
+        words=True,
+        magic=True,
+        morefeatures=['chapter'],
+        worker=WORKER,
+        pages=':',
+    )
