4d1d4e9ab2723b3f388f5b055b9cc3f884b493e2
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-13 14:32:45 +0100

    test(cli): verify german document decorator

--- tests/chapter/test_cli.py
+++ tests/chapter/test_cli.py
@@ -7,6 +7,8 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import power
+import protocol
 import utila
 
 import decider_chapter
@@ -19,3 +21,18 @@ def test_decider_content_cli_help(monkeypatch):
 
 def test_decider_content_nomonkey_cli_help():
     utila.run(f'{decider_chapter.PROCESS} --help')
+
+
+def test_language_decorator(testdir, monkeypatch):
+    """Disable 6500 for other language than german."""
+    source = power.link(power.MASTER075_PDF)
+    german = testdir.tmpdir.join('german')
+    tests.chapter.run(f'-i {source} -o {german}', monkeypatch=monkeypatch)
+    germans = protocol.findings_from_path(german, msgid=6500)
+    assert germans
+    # skip check for english lang
+    english = testdir.tmpdir.join('english')
+    cmd = f'-i {source} -o {english} --docinfo=english'
+    tests.chapter.run(cmd, monkeypatch=monkeypatch)
+    englishs = protocol.findings_from_path(english, msgid=6500)
+    assert not englishs, 'is english supported now?'
