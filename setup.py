c8b9abf7975f349e8ff43721767fbc782e0752da
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-06-21 12:30:19 +0200

    chore(setup): cleanup setup

--- setup.py
+++ setup.py
@@ -14,18 +14,15 @@ import re
 import setuptools
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
-UTF8 = 'utf8'
 
-with open(os.path.join(ROOT, 'README.md'), mode='rt', encoding=UTF8) as fp:
+with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
     README = fp.read()
 
-with open(os.path.join(ROOT, 'decider_con/__init__.py'), 'rt', encoding=UTF8) as fp: # yapf:disable
+with open(os.path.join(ROOT, 'decider_con/__init__.py'),  encoding='utf8') as fp: # yapf:disable
     VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
 
-with open(os.path.join(ROOT, "requirements.txt"), 'rt', encoding=UTF8) as fp:
-    INSTALL_REQUIRES = [
-        line for line in fp.readlines() if line and '#' not in line
-    ]
+with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
+    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
 
 if __name__ == "__main__":
     # allow ssetup.py to run from another directory
@@ -34,7 +31,7 @@ if __name__ == "__main__":
         author='Helmut Konrad Fahrendholz',
         author_email='info@checkitweg.de',
         description='judge content',
-        install_requires=INSTALL_REQUIRES,
+        install_requires=REQUIRES,
         long_description=README,
         name='decider_con',
         platforms='any',
