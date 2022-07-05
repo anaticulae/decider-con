243faf6ae477386041d0c9e1af21b3e321820738
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-07-05 20:16:12 +0200

    chore(resources): remove duplicated resource

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,8 +31,6 @@ RESOURCES = [
     (power.DOCU035_PDF, '0:10'),
     (power.HOME050_PDF, '30:40'),
     (power.MASTER063_PDF, '20:30'),
-    (power.MASTER072_PDF, '0:10'),
-    (power.MASTER078_PDF, '0:10'),
     (power.MASTER078_PDF, '0:10'),
     (power.MASTER083_PDF, '0:10'),
     (power.MASTER098_PDF, '0:10,43:65,88:97'),
